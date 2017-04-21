3a58fca84e16c2331c63806de20bc69a8ed10ef5
logicfeel <logicfeel@nate.com>
2017-04-21 18:35:00 +0900

    일단 작업중지.. 회식..고

--- index.js
+++ index.js
@@ -3,48 +3,9 @@
 (function(global) {
     'use strict';    
 
-    var entityNamespace = "M01.Notice";
-    var entity = {
-        tables: [
-            {
-                name: "Notice",
-                columns: [
-                    {
-                        name: "ntc_idx",
-                        datatype: "string",
-                        caption: "일련번호"
-                    },
-                    {
-                        name: "noticeType_cd",
-                        datatype: "string",
-                        caption: "일련번호"
-                    },
-                    {
-                        name: "title",
-                        datatype: "string",
-                        caption: "제목"
-                    },
-                    {
-                        name: "writer",
-                        datatype: "string",
-                        caption: "작성자"
-                    },
-                    {
-                        name: "create_dt",
-                        datatype: "string",
-                        caption: "등록일자"
-                    },
-                    {
-                        name: "cnt",
-                        datatype: "string",
-                        caption: "조회수"
-                    }
-                ]
-            }
-        ]
-    };
+    
 
-    global.Entity                   = global.Entity || {};
-    global.Entity[entityNamespace]  = global.Entity[entityNamespace] || entity;
+    // global.Entity                   = global.Entity || {};
+    // global.Entity[entityNamespace]  = global.Entity[entityNamespace] || entity;
 
 }(this));
---
+++ template_schema.js
@@ -0,0 +1,35 @@
+
+/**
+ * 템플릿 스키마
+ *  - 요구사항
+ *      + handlebar 와 의 연동할 경우 객체의 스키마 타입 정의
+ */
+
+(function(global) {
+    'use strict';    
+
+    function template_schema() {
+
+    }
+
+    function schema() {}
+
+    /**
+     * 테이블 정보
+     *  - 테이블 이름, 갯수 ?
+     *  - 컬럼 이름, 갯수
+     * param 정보
+     * 기본 설정
+     * 동작 (기능)
+     * 스타일
+     * 필수/선택
+     * 
+     * 추상적 상위 스키마 또는 하위 템플릿
+     *  - 공통의 묶음영역
+     *  - 재귀적 호출 사용 방법?
+     */
+
+    // global.Entity                   = global.Entity || {};
+    // global.Entity[entityNamespace]  = global.Entity[entityNamespace] || entity;
+
+}(this));
