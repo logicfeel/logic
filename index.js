746562a8bb27d1a3ec15a40abbf88cf2fff946e6
logicfeel <logicfeel@nate.com>
2017-04-25 11:25:57 +0900

    innner js 추가

--- index.js
+++ index.js
@@ -3,12 +3,20 @@
 
     function npmtest() {
         console.log('run... npmtest');
+
+        var test = require('./inner.js');
+
     }
 
+    
+    // require('npmtest'); 이런식으로 불러 올수 있게 함
     if (typeof module !== 'undefined' && module.exports) {
         module.exports.npmtest = npmtest;
     } else {
         global.npmtest = npmtest;
     }
+
+
+    // npmtest();
 
 }(this));
